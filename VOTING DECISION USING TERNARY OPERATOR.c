8470624a0f6648d78223887229686e371ba1b954
AKSHAY S KASHYAP <85955711+ask003@users.noreply.github.com>
2021-10-08 15:32:06 +0530

    Add files via upload

---
+++ VOTING DECISION USING TERNARY OPERATOR.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int age,a=18;
+  printf("ENTER YOUR AGE: ");
+  scanf("%d",&age);
+  age>=a?printf("ELIGIBLE TO VOTE"):printf( "NOT ELIGIBLE TO VOTE,WAIT UNTIL YOUR AGE IS 18");
+
+    return 0;
+}
